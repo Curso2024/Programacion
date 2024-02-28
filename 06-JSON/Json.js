35cf8a51a47088939e26b5d6167cbd97c746e5d6
Curso2024 <161453381+Curso2024@users.noreply.github.com>
2024-02-28 13:54:14 +0100

    hh

---
+++ 06-JSON/Json.js
@@ -0,0 +1,21 @@
+var fabrica = {
+    departamentos: [
+        {
+            nombre: "Finanzas",
+            telefono: "919-521-3652",
+            empleados: [
+                { nombre: "Adriana", apellido: "Ulloa", edad: 25 },
+                { nombre: "Raul", apellido: "Viera", edad: 65 },
+            ],
+        },
+        {
+            nombre: "Ventas",
+            telefono: "658-854-7485",
+            empleados: [
+                { nombre: "Perla", apellido: "Straz", edad: 22 },
+                { nombre: "Roman", apellido: "Dosser", edad: 50 },
+                { nombre: "Narda", apellido: "Fezze", edad: 62 },
+            ],
+        },
+    ],
+};
